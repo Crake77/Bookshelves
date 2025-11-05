3fc4c96a61fb459e3d55dc7137bfc831bafd6414
John Dunham <john.david.dunham@gmail.com>
2025-11-05 10:16:49 -0500

    fix: JSX structure in CoverCarouselDialog

--- client/src/components/CoverCarouselDialog.tsx
+++ client/src/components/CoverCarouselDialog.tsx
@@ -275,6 +275,7 @@ export default function CoverCarouselDialog({
                 </div>
               </div>
             ))}
+            </div>
           </div>
 
           {/* Navigation Buttons */}
